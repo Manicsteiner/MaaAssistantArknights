b89322732f7b646526c97fddebb71888be1fdeb1
ManicSteiner <manicsteiner@outlook.com>
2024-07-09 00:04:21 +0800

    feat: extra fixes

--- src/MaaCore/Task/Interface/RoguelikeTask.cpp
+++ src/MaaCore/Task/Interface/RoguelikeTask.cpp
@@ -104,16 +104,18 @@ bool asst::RoguelikeTask::set_params(const json::value& params)
     m_roguelike_config_ptr->set_difficulty(0);
     // 是否凹指定干员开局直升
     m_roguelike_config_ptr->set_start_with_elite_two(params.get("start_with_elite_two", false));
-    m_roguelike_config_ptr->set_only_start_with_elite_two(params.get("only_start_with_elite_two", false));
+    m_roguelike_config_ptr->set_only_start_with_elite_two(
+        params.get("only_start_with_elite_two", false));
     // 是否凹开局远见密文板
     m_roguelike_config_ptr->set_first_floor_foldartal(params.contains("first_floor_foldartal"));
     m_roguelike_config_ptr->set_start_floor_foldartal(params.get("first_floor_foldartal", ""));
 
     // 是否生活队凹开局板子
     m_roguelike_config_ptr->set_start_foldartal(params.contains("start_foldartal_list"));
-    
+
     // 是否跳过阵容完备度检测
-    m_roguelike_config_ptr->set_recruitment_team_complete(params.get("skip_team_completeness_check", false));
+    m_roguelike_config_ptr->set_recruitment_team_complete(
+        params.get("skip_team_completeness_check", false));
 
     if (auto opt = params.find<json::array>("start_foldartal_list"); opt) {
         std::vector<std::string> list;
@@ -130,14 +132,21 @@ bool asst::RoguelikeTask::set_params(const json::value& params)
     }
 
     // 是否使用密文版, 非CLP_PDS模式下默认为True, CLP_PDS模式下默认为False
-    m_roguelike_config_ptr->set_use_foldartal(params.get("use_foldartal", !(mode == RoguelikeMode::CLP_PDS)));
+    m_roguelike_config_ptr->set_use_foldartal(
+        params.get("use_foldartal", !(mode == RoguelikeMode::CLP_PDS)));
 
-    // 是否检查坍缩范式，非CLP_PDS模式下默认为False, CLP_PDS模式下默认为True
-    m_roguelike_config_ptr->set_check_clp_pds(params.get("check_collapsal_paradigms", mode == RoguelikeMode::CLP_PDS));
-    m_roguelike_config_ptr->set_double_check_clp_pds(params.get("double_check_collapsal_paradigms", mode == RoguelikeMode::CLP_PDS));
+    if (theme == RoguelikeTheme::Sami) {
+        // 是否检查坍缩范式，非CLP_PDS模式下默认为False, CLP_PDS模式下默认为True
+        m_roguelike_config_ptr->set_check_clp_pds(
+            params.get("check_collapsal_paradigms", mode == RoguelikeMode::CLP_PDS));
+        m_roguelike_config_ptr->set_double_check_clp_pds(
+            params.get("double_check_collapsal_paradigms", mode == RoguelikeMode::CLP_PDS));
 
-    const std::unordered_set<std::string>& rare_clp_pds = RoguelikeCollapsalParadigms.get_rare_clp_pds(theme);
-    m_roguelike_config_ptr->set_expected_clp_pds(params.get("expected_collapsal_paradigms", rare_clp_pds));
+        const std::unordered_set<std::string>& rare_clp_pds =
+            RoguelikeCollapsalParadigms.get_rare_clp_pds(theme);
+        m_roguelike_config_ptr->set_expected_clp_pds(
+            params.get("expected_collapsal_paradigms", rare_clp_pds));
+    }
 
     m_roguelike_config_ptr->set_invest_maximum(params.get("investments_count", INT_MAX));
     m_roguelike_config_ptr->set_invest_stop_when_full(params.get("stop_when_investment_full", false));
