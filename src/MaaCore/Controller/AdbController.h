1a026a25ebbe97a1805fde36b8ca01cc7721d452
Manicsteiner <Manicsteiner@outlook.com>
2026-04-28 17:19:05 +0800

    feat: AVD 截图增强 旧版

--- src/MaaCore/Controller/AdbController.h
+++ src/MaaCore/Controller/AdbController.h
@@ -7,6 +7,7 @@
 
 #include "Platform/PlatformFactory.h"
 
+#include "AVDExtras.h"
 #include "Common/AsstMsg.h"
 #include "Config/GeneralConfig.h"
 #include "InstHelper.h"
@@ -201,6 +202,7 @@ protected:
             MumuExtras,
             LDExtras,
 #endif
+            AVDExtras,
         } screencap_method = ScreencapMethod::UnknownYet;
     } m_adb;
 
@@ -222,5 +224,6 @@ protected:
     MumuExtras m_mumu_extras;
     LDExtras m_ld_extras;
 #endif
+    AVDExtras m_avd_extras;
 };
 } // namespace asst
